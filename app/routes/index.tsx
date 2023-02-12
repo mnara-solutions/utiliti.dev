b855222ed22885b5a94ff580264657f9fe98379c
Hussein Jafferjee <hussein@jafferjee.ca>
2023-02-12 14:55:44 -0800

    clean up messaging

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -16,7 +16,14 @@ export default function Index() {
       <p>
         <ul>
           <li>A set of fast and well-designed utilities.</li>
-          <li>No data will be sent back to us, except for private notes.</li>
+          <li>
+            Use local computation where possible so that no data is ever sent
+            back to us.
+          </li>
+          <li>
+            If data needs to be sent to us (private notes), it will be
+            end-to-end encrypted.
+          </li>
           <li>No tracking.</li>
         </ul>
       </p>
