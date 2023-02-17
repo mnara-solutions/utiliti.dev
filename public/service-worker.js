34ff2659749e26b48597d1cc6e6e39ca7468b7dd
Hussein Jafferjee <hussein@jafferjee.ca>
2023-02-17 09:46:21 -0800

    also start caching documents

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,6 @@
 const STATIC_ASSETS = ["/build/", "/assets/"];
+const ASSET_CACHE = "assets";
+const DOCUMENT_CACHE = "documents";
 
 /**
  * Very simple service worker that caches the assets and serves them if they exist in the cache.
@@ -7,36 +9,56 @@ const STATIC_ASSETS = ["/build/", "/assets/"];
  * a typescript worker. Look at `remix-pwa` and see how they do it.
  */
 self.addEventListener("fetch", (event) => {
-  // we are only caching static assets
-  if (
-    event.request.method.toLowerCase() !== "get" ||
-    !STATIC_ASSETS.some((publicPath) =>
-      event.request.url.startsWith(publicPath)
-    )
-  ) {
+  if (event.request.method !== "GET") {
     return;
   }
 
-  event.respondWith(
-    (async () => {
-      const cached = await caches.match(event.request, {
-        cacheName: "assets",
-        ignoreVary: true,
-        ignoreSearch: true,
-      });
+  event.respondWith((async () => await handleFetch(event))());
+});
 
-      if (cached) {
-        return cached;
-      }
+async function handleFetch(event) {
+  const r = event.request;
 
+  // cache any static assets
+  // serve straight away from a cache
+  if (STATIC_ASSETS.some((publicPath) => r.url.startsWith(publicPath))) {
+    const cached = await caches.match(r, {
+      cacheName: ASSET_CACHE,
+      ignoreVary: true,
+      ignoreSearch: true,
+    });
+
+    if (cached) {
+      return cached;
+    }
+
+    const response = await fetch(r);
+
+    if (response.status === 200) {
+      const cache = await caches.open(ASSET_CACHE);
+      await cache.put(r, response.clone());
+    }
+
+    return response;
+  }
+
+  // cache any documents
+  // we attempt to fetch via the network first, and fallback to cache incase offline
+  if (r.mode === "navigate") {
+    try {
       const response = await fetch(event.request);
-
-      if (response.status === 200) {
-        const cache = await caches.open("assets");
-        await cache.put(event.request, response.clone());
-      }
+      const cache = await caches.open(DOCUMENT_CACHE);
+      await cache.put(event.request, response.clone());
 
       return response;
-    })()
-  );
-});
+    } catch (error) {
+      const response = await caches.match(event.request);
+
+      if (response) {
+        return response;
+      }
+
+      throw error;
+    }
+  }
+}
