08b9aabb71e0b0980a4de8e730020148dd1fd7b8
Hussein Jafferjee <hussein.jafferjee@verygoodsecurity.com>
2023-03-18 18:29:29 -0700

    fix google ad

--- app/components/google-ad.tsx
+++ app/components/google-ad.tsx
@@ -1,30 +1,21 @@
-import { ClientOnly } from "~/components/client-only";
+import { useEffect } from "react";
 
 export default function GoogleAd() {
+  useEffect(() => {
+    // @ts-ignore
+    (adsbygoogle = window.adsbygoogle || []).push({});
+  }, []);
+
   return (
-    <ClientOnly>
-      {() => (
-        <div className="xl:max-w-none not-prose mt-16">
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7171176134175232"
-            crossOrigin="anonymous"
-          ></script>
-          <ins
-            className="adsbygoogle"
-            style={{ display: "block" }}
-            data-ad-client="ca-pub-7171176134175232"
-            data-ad-slot="5476284816"
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-          ></ins>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(adsbygoogle = window.adsbygoogle || []).push({});`,
-            }}
-          />
-        </div>
-      )}
-    </ClientOnly>
+    <div className="xl:max-w-none not-prose mt-16">
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-7171176134175232"
+        data-ad-slot="5476284816"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+    </div>
   );
 }
