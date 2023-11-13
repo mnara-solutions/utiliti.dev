daca3793450145119d46f265ea8f21160b0575c1
kris <kristian.erickson@gmail.com>
2023-11-13 14:38:03 -0800

    Add output format and quality to dataUrl.

--- app/components/read-file.tsx
+++ app/components/read-file.tsx
@@ -7,9 +7,59 @@ interface Props {
   readonly accept: string;
   readonly onLoad: (value: string) => void;
   readonly type?: "text" | "dataURL";
+  readonly format?: string;
+  readonly quality?: string;
 }
 
-export default function ReadFile({ accept, onLoad, type = "text" }: Props) {
+const imageFormats: Record<string, string> = {
+    "image/jpeg": "jpg",
+    "image/png": "png",
+    "image/webp": "webp",
+};
+
+const formatImage: Record<string, string> = {
+     "jpg" :"image/jpeg",
+     "png" : "image/png",
+    "webp":"image/webp",
+};
+
+function convertToFileFormat(file: File, format: string, quality:number):Promise<string> {
+    return new Promise((resolve:(dataUrl:string) => void, reject:(error:string) => void):void => {
+        const img = new Image();
+
+        // Set up an onload event handler to execute the conversion when the image is loaded
+        img.onload = () => {
+            // Create a canvas element
+            const canvas = document.createElement('canvas');
+            canvas.width = img.width;
+            canvas.height = img.height;
+
+            // Get the 2D context of the canvas
+            const ctx = canvas.getContext('2d');
+
+            if (ctx) {
+                // Draw the image onto the canvas
+                ctx.drawImage(img, 0, 0);
+            } else {
+                reject("Unable to get context");
+            }
+
+            // Convert the canvas content to a data URL (PNG format)
+            if (quality !== 0) {
+                resolve( canvas.toDataURL(formatImage[format], quality));
+            } else {
+                resolve( canvas.toDataURL(formatImage[format]));
+            }
+
+
+        };
+
+        // Set the source of the image to the JPEG file
+        img.src = URL.createObjectURL(file);
+    });
+}
+
+export default function ReadFile({ accept, onLoad, type = "text", format = "jpg", quality = "0" }: Props) {
   const onButtonClick = useCallback(
     () => document.getElementById("file-input")?.click(),
     [],
@@ -42,11 +92,21 @@ export default function ReadFile({ accept, onLoad, type = "text" }: Props) {
           reader.readAsText(file);
           break;
         case "dataURL":
-          reader.readAsDataURL(file);
+          const fileFormat = imageFormats[file.type];
+          if ((fileFormat && fileFormat !== format) || (quality != "0" && format !== 'png')) {
+              convertToFileFormat(file, format || "jpg", parseFloat(quality)).then(dataUrl => {
+                  onLoad(dataUrl);
+              }).catch(_ => {
+                  console.error('Something went wrong, trying plain old read as dataurl');
+                  reader.readAsDataURL(file);
+              })
+          } else {
+              reader.readAsDataURL(file);
+          }
           break;
       }
     },
-    [onLoad, type],
+    [onLoad, type, format, quality],
   );
 
   return (
