86618807e237b50f2717f81c4dbfe5bc24c6d19b
Hussein Jafferjee <hussein.jafferjee@verygoodsecurity.com>
2023-11-14 15:47:21 -0800

    refactor callback

--- app/components/dropdown.tsx
+++ app/components/dropdown.tsx
@@ -1,23 +1,36 @@
-import type { SelectHTMLAttributes } from "react";
+import type { ChangeEvent, SelectHTMLAttributes } from "react";
+import { useCallback } from "react";
 import { classNames } from "~/common";
 
 interface Props extends SelectHTMLAttributes<HTMLSelectElement> {
   readonly options: { readonly id: string; readonly label: string }[];
   readonly onOptionChange: (value: string) => void;
 }
-export default function Dropdown(props: Props) {
+export default function Dropdown({
+  name,
+  id,
+  onOptionChange,
+  options,
+  className,
+  defaultValue,
+}: Props) {
+  const onChange = useCallback(
+    (e: ChangeEvent<HTMLSelectElement>) => onOptionChange(e.target.value),
+    [onOptionChange],
+  );
+
   return (
     <select
-      id={props.id}
-      name={props.name}
+      id={id}
+      name={name}
       className={classNames(
         "block text-sm border rounded-lg bg-zinc-700 border-zinc-600 placeholder-zinc-400 focus:ring-orange-500 focus:border-orange-500",
-        props.className,
+        className,
       )}
-      defaultValue={props.defaultValue}
-      onChange={(e) => props.onOptionChange(e.target.value)}
+      defaultValue={defaultValue}
+      onChange={onChange}
     >
-      {props.options.map((it) => (
+      {options.map((it) => (
         <option key={it.id} value={it.id}>
           {it.label}
         </option>
