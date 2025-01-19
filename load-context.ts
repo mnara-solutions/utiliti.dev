f16a64954d3fb90cae6be35bd51dc9d7b32a9d61
Hussein Jafferjee <hussein@jafferjee.ca>
2025-01-19 11:42:31 -0800

    trying to make it work with cloudflare-pages

--- load-context.ts
+++ load-context.ts
@@ -1,9 +1,26 @@
+import { KVNamespace } from "@cloudflare/workers-types";
+import { type AppLoadContext } from "@react-router/cloudflare";
 import { type PlatformProxy } from "wrangler";
 
+type Env = {
+  PRIVATE_NOTES: KVNamespace;
+  CF_WHOIS_KEY: string;
+};
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
 
-declare module "@remix-run/cloudflare" {
+declare module "@react-router/cloudflare" {
   interface AppLoadContext {
     cloudflare: Cloudflare;
   }
 }
+
+type GetLoadContext = (args: {
+  request: Request;
+  context: { cloudflare: Cloudflare };
+}) => AppLoadContext;
+
+export const getLoadContext: GetLoadContext = ({ context }) => {
+  return {
+    ...context,
+  };
+};
