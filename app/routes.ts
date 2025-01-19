fc44a31899af3edcfb39b080f979bc44a6defef3
Hussein Jafferjee <hussein@jafferjee.ca>
2025-01-19 10:55:24 -0800

    fix up flat routes for prod build

--- app/routes.ts
+++ app/routes.ts
@@ -25,4 +25,4 @@ export class Routes {
   static readonly HASHING = "/hashing";
 }
 
-export default [...(await flatRoutes())] satisfies RouteConfig;
+export default flatRoutes() satisfies RouteConfig;
