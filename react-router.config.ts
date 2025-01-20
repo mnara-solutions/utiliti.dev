2b7f60c66d0124643b64bf81f663287fd7957884
Hussein Jafferjee <hussein@jafferjee.ca>
2025-01-19 20:38:20 -0800

    pre-rendering

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "@react-router/dev/config";
+import { utilities } from "./app/utilities";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  async prerender() {
+    return Object.values(utilities).map((it) => it.url);
+  },
 } satisfies Config;
