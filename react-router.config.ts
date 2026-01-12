3655e505c76a6e68072915440f955fbb497215ff
Hussein Jafferjee <hussein@jafferjee.ca>
2026-01-11 16:14:36 -0800

    pre-render differently

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,9 +1,6 @@
 import type { Config } from "@react-router/dev/config";
-import { utilities } from "./app/utilities";
 
 export default {
   ssr: true,
-  // async prerender() {
-  //   return ["/", ...Object.values(utilities).map((it) => it.url)];
-  // },
+  prerender: true,
 } satisfies Config;
