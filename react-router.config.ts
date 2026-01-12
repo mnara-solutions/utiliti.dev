7d21f855e312c2d6e9a4428411f514984a7fa66f
Hussein Jafferjee <hussein@jafferjee.ca>
2026-01-11 16:12:21 -0800

    disable prerendering as a test

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,8 +3,7 @@ import { utilities } from "./app/utilities";
 
 export default {
   ssr: true,
-  async prerender() {
-    // Prerender all utility pages for faster initial paint
-    return ["/", ...Object.values(utilities).map((it) => it.url)];
-  },
+  // async prerender() {
+  //   return ["/", ...Object.values(utilities).map((it) => it.url)];
+  // },
 } satisfies Config;
