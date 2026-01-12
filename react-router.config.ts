b71b8fb82e3919c787f0a5b86656fc73b4b7dcc5
Hussein Jafferjee <hussein@jafferjee.ca>
2026-01-11 16:32:56 -0800

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
-  async prerender() {
+  //async prerender() {
     // return ["/", ...Object.values(utilities).map((it) => it.url)];
-  },
+  //},
 } satisfies Config;
