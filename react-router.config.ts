3c2a5d67fd50ad021b32662984718657fc2cac33
Hussein Jafferjee <hussein@jafferjee.ca>
2026-01-11 16:38:21 -0800

    move to cloudflare workers

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,13 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
+<<<<<<< Updated upstream
   // async prerender() {
   //   return Object.values(utilities).map((it) => it.url);
   // },
+=======
+  prerender: [],
+  // To enable prerendering, uncomment and adjust:
+  // prerender: ["/", ...Object.values(utilities).map((it) => it.url)],
+>>>>>>> Stashed changes
 } satisfies Config;
