68e22f1f2ccc289ec4a020b72b113d2e1f3293a7
Hussein Jafferjee <hussein@jafferjee.ca>
2026-01-18 18:41:42 -0800

    mistaken commit

--- dist/client/assets/convert-text-file-DJUzPpLu.js
+++
@@ -1,1 +0,0 @@
-function n(t){return Promise.all(t.map(e=>e.text()))}async function o(t,e){e((await n(t))[0])}export{o as s};
--- dist/client/assets/meta-DjZvpH_S.js
+++
@@ -1,1 +0,0 @@
-function o(n,t){const e=`${n} | Utiliti`;return()=>[{title:e},{name:"description",content:t},{property:"og:title",content:e},{property:"og:description",content:t}]}export{o as m};
